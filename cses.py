3027182c7acf9dba1391e562a3bbdd65bbdb4466
Suraj chauhan <surajssc1232@gmail.com>
2024-12-20 15:30:13 +0530

    idk

--- cses.py
+++ cses.py
@@ -1,15 +1,9 @@
-n=int(input())
-nums = [int(x) for x in input().split()][:n]
+from flask import Flask, request
 
-# 3 2 5 1 7
-count = 0
-for i in range(1, len(nums)-1):
-    if not nums[i-1] < nums[i] < nums[i+1]:
-        while nums[i] <= nums[i-1] or nums[i] >= nums[i+1]:
-            nums[i] += 1
-            count += 1
-
-print(count)
+app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return "Hello World!"
 
