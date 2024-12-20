7de1aba92a690b81872bc96bcd65ad9a7349c25e
Suraj chauhan <surajssc1232@gmail.com>
2024-12-20 14:21:17 +0530

    asksjdn

--- cses.py
+++ cses.py
@@ -1,15 +1,2 @@
-n=int(input())
-nums = [int(x) for x in input().split()][:n]
-
-# 3 2 5 1 7
-count = 0
-for i in range(1, len(nums)-1):
-    if not nums[i-1] < nums[i] < nums[i+1]:
-        while nums[i] <= nums[i-1] or nums[i] >= nums[i+1]:
-            nums[i] += 1
-            count += 1
-
-print(count)
-
-
+print('main branch')
 
